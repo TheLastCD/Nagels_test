a72ad6eb86112ca80cf37677eebbba277ad6fcbb
TheLastCD <34370996+TheLastCD@users.noreply.github.com>
2023-04-24 14:32:50 +0100

    completed splitter code and binary conversion
    both methods need slight optimisation but overall works accurately so far.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace Nagels_Test
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string hex1 = "A4 E6 16 76 56 C7 32 05 54 B2 04 C7 46 42 E2 E2 E2 E2 E2 E2 E3 9C 15 AF 43";
+            HexDecode test = new HexDecode(hex1);
             Console.WriteLine("Hello World!");
         }
     }
@@ -15,26 +18,49 @@ namespace Nagels_Test
     //     all 4 bools/TextA                                                      /ShortA/DateTimeA
     class HexDecode
     {
-        public string data;
-        public string[] split;
-        public bool[] bitABCD;
+        public List<char> data;
+        public List<List<char>> split = new List<List<char>>();
+        public List<bool> bitABCD = new List<bool>();
         
         public HexDecode(string unedited){
-            data = unedited;
-            split  = new string[3];
-            bitABCD = new bool[2];
+            string[] x = unedited.Split(' ');
+            data = new List<char>();
+            foreach(string i in x){
+                char[] temp = i.ToCharArray();
+                data.Add(temp[0]);
+                data.Add(temp[1]);
+            }
+            Splitter();
+            BoolConn();
         }
 
         // For splitting and appropriately formatting the input into:
         //[bitsA-D,TextA,ShortA,DateTimeA]
-        public string[] Splitter(){
+        public void Splitter(){
+            //range set for the split
+            int[] ranges = new int[8]{0,0,1,40,41,44,45,49};
+            
+            List<char> subList = new List<char>();
 
-            return new string[] {"hello"};
+            //loop through every other entry in ranges
+            for (int i = 0; i < ranges.Length; i= i+2){
+                //break at last value
+                if(ranges[i] == 49){break;}
+                //slice list to within the range i and i+1
+                subList = (data.Where((value, index) => index >= ranges[i] && index <= ranges[i+1]).ToList());
+                //Append it to a 2 dimensional list for further work
+                split.Add(subList);
+                
+            }
         }
 
-        //convert the first item in split array 
+        //convert the first digit into the four bools
         public void BoolConn(){
-
+            //convert the digit into a binary nibble
+            string binarystring = String.Join(String.Empty,split[0].Select
+            (c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')));
+            //append the individual bits to the ABCD Array for future use
+            foreach(char x in binarystring.ToCharArray()){ bitABCD.Add(Convert.ToBoolean(Convert.ToInt32(new string(x, 1))));}
         }
         //convert hex to text
         public void TextConn(){
